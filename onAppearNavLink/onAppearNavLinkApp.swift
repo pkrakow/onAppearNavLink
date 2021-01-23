6b346c728a11cd664729de1a431b8a5b53ab39a4
Paul Krakow <pkrakow@gmail.com>
2021-01-23 10:45:10 -0800

    Initial Commit

---
+++ onAppearNavLink/onAppearNavLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  onAppearNavLinkApp.swift
+//  onAppearNavLink
+//
+//  Created by Paul on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct onAppearNavLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
